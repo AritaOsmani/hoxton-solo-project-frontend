563494211bd96239d45e38fe8fcc403af1497b39
Arita Osmani <aritaaosmani@gmail.com>
2022-04-06 15:31:12 +0200

    Added replies function-no styling

--- src/components/ExpandedCommentItem.tsx
+++ src/components/ExpandedCommentItem.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import '../styles/ExpandedCommentItem.css'
 import { Comment, User } from '../Types'
 
@@ -6,9 +7,23 @@ type Props = {
     postComment: Comment,
     setReplyingTo: React.Dispatch<React.SetStateAction<User | null>>
     setInputValue: React.Dispatch<React.SetStateAction<string>>
+    setCommentReplying: React.Dispatch<React.SetStateAction<number>>
+    repliesForComment: Comment[],
+    setRepliesForComment: React.Dispatch<React.SetStateAction<Comment[]>>
 }
 
-export default function ExpandedCommentItem({ postComment, setReplyingTo, setInputValue }: Props) {
+export default function ExpandedCommentItem({ postComment, setReplyingTo, setInputValue, setCommentReplying, repliesForComment, setRepliesForComment }: Props) {
+    useEffect(() => {
+
+        fetch(`http://localhost:4000/commentReplies/${postComment.id}`).then(res => res.json()).then(data => {
+            if (data.error) {
+                alert(data.error)
+            } else {
+                setRepliesForComment(data)
+            }
+        })
+
+    }, [repliesForComment])
     return (
         <div className='expanded-page-comment'>
             <img src={postComment.user.image} alt="" />
@@ -16,7 +31,11 @@ export default function ExpandedCommentItem({ postComment, setReplyingTo, setInp
             <button onClick={() => {
                 setReplyingTo(postComment.user)
                 setInputValue(`@${postComment.user.username} `)
+                setCommentReplying(postComment.id)
             }} className='reply-btn'>reply</button>
+            <ul className='replies-section'>
+                {repliesForComment.map(reply => <li>{reply.content}</li>)}
+            </ul>
         </div>
     )
 }

93d588111bf04901709163e0e56f4871073dbcaf
Arita Osmani <aritaaosmani@gmail.com>
2022-04-06 11:19:42 +0200

    Made side menu stricky

--- src/components/SideAccountMenu.tsx
+++ src/components/SideAccountMenu.tsx
@@ -31,25 +31,28 @@ export default function SideAccountMenu({ user }: Props) {
 
     return (
         <div className='side-account-menu-container'>
-            <div className='user-info' onClick={() => {
-                navigate(`/${user?.username}`)
-            }}>
-                <img src={user?.image} alt="" />
-                <div className='bio-username'>
-                    <span className='username'>{user?.username}</span>
-                    <span className='bio'>{user?.bio}</span>
-                </div>
+            <div className='side-account-wrapper'>
+                <div className='user-info' onClick={() => {
+                    navigate(`/${user?.username}`)
+                }}>
+                    <img src={user?.image} alt="" />
+                    <div className='bio-username'>
+                        <span className='username'>{user?.username}</span>
+                        <span className='bio'>{user?.bio}</span>
+                    </div>
 
+                </div>
+                <div className='suggestion-link'>
+                    <h3>Suggestions For You</h3>
+                    <span>See All</span>
+                </div>
+                <ul className='suggested-users'>
+                    {/* {suggested.map(s => <SuggestedUser key={s.id} suggestedUser={s} />)} */}
+                    {getOnlyFive(suggested).map(s => <SuggestedUser key={s.id} suggestedUser={s} suggested={suggested} setSuggestedUser={setSuggested} />)}
+                </ul>
+                <MainPageFooter />
             </div>
-            <div className='suggestion-link'>
-                <h3>Suggestions For You</h3>
-                <span>See All</span>
-            </div>
-            <ul className='suggested-users'>
-                {/* {suggested.map(s => <SuggestedUser key={s.id} suggestedUser={s} />)} */}
-                {getOnlyFive(suggested).map(s => <SuggestedUser key={s.id} suggestedUser={s} suggested={suggested} setSuggestedUser={setSuggested} />)}
-            </ul>
-            <MainPageFooter />
+
         </div>
     )
 }
